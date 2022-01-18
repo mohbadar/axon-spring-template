552b6b4bf19b84dcf103da442a123a8153732e31
Mohammad Badar Hashimi <32482095+mohbadar@users.noreply.github.com>
2022-01-18 10:19:30 +0430

    Axon Spring

---
+++ src/main/java/com/unite/axon_spring/AxonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unite.axon_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unite/axon_spring/AxonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unite.axon_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
